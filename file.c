f42e94b59030be1a775af0ee2d582d87dcf30da6
Choi Jae-won <jwonee1343@gmail.com>
2018-11-23 17:22:28 +0900

    Update file.c

--- file.c
+++ file.c
@@ -1,10 +1,10 @@
+#include <malloc.h>
+#include <unistd.h>
 #include <sys/stat.h>
 
 #define FILE_SIZE(fd) \
 	({ struct stat s; fstat(fd, &s) < 0 ? -1 : s.st_size; })
 
-#include <unistd.h>
-#include <malloc.h>
 
 void
 file_copy(int src, int cp, long offset)
@@ -13,16 +13,15 @@ file_copy(int src, int cp, long offset)
 	size_t leftover;
 
 	buf = malloc(offset);
+	
 	for (leftover = FILE_SIZE(src);
 	     leftover >= offset; 
-	     leftover = leftover - offset)
-	{
+	     leftover = leftover - offset) {
 		read(src, buf, offset);
 		write(cp, buf, offset);
 	}
-
 	read(src, buf, leftover);
 	write(cp, buf, leftover);
-
+	
 	free(buf);
 }
